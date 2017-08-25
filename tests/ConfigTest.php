de1c8b7f8ecf065d73e4ecf9ee65a436e970a480
Aldarien <jpvial@gmail.com>
2017-08-25 16:33:15 -0300

    Changed test to include creation of test config file

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -3,11 +3,22 @@ use PHPUnit\Framework\TestCase;
 
 class ConfigTest extends TestCase
 {
+	public function setUp()
+	{
+		mkdir(dirname(__DIR__) . '/config');
+		$str = "<?php return ['name' => 'Config']; ?>";
+		file_put_contents(dirname(__DIR__) . '/config/app.php', $str);
+	}
 	public function testGetName()
 	{
 		$name = 'Config';
 		
 		$this->assertEquals($name, config('app.name'));
+	}
+	public function tearDown()
+	{
+		unlink(dirname(__DIR__) . '/config/app.php');
+		rmdir(dirname(__DIR__) . '/config');
 	}
 }
 ?>
