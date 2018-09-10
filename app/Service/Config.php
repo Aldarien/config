14a320198298039ea1498007698d2dc8766dec51
Aldarien <aldarien85@gmail.com>
2018-09-10 16:19:47 -0300

    Spaces

--- app/Service/Config.php
+++ app/Service/Config.php
@@ -21,30 +21,30 @@ class Config
 		$files = $this->getFiles($this->dir);
 		foreach ($files as $file) {
 			$name = $file->name;
-      $data = $this->getData($file);
-      $this->set($name, $data);
+			$data = $this->getData($file);
+			$this->set($name, $data);
 		}
 		$this->checkValues();
 	}
 	protected function getFiles($location)
-  {
-    $files = [];
-    $d = new \DirectoryIterator($location) or die("getFileList: Failed opening directory $location for reading");
-    foreach ($d as $fileinfo) {
-      if ($fileinfo->isDot() or $fileinfo->isDir()) {
-        continue;
-      }
+	{
+		$files = [];
+		$d = new \DirectoryIterator($location) or die("getFileList: Failed opening directory $location for reading");
+		foreach ($d as $fileinfo) {
+			if ($fileinfo->isDot() or $fileinfo->isDir()) {
+				continue;
+			}
 			if (!preg_match("/\.(php|json|yml)*$/i", $fileinfo->getFilename(), $matches)) {
-			  continue;
+				continue;
 			}
 			$files []= (object) [
-        'filename' => "{$location}{$fileinfo}",
-        'name' => strtolower($fileinfo->getBasename('.' . $fileinfo->getExtension())),
-        'type' => strtolower($fileinfo->getExtension())
-      ];
-    }
-    return $files;
-  }
+				'filename' => "{$location}{$fileinfo}",
+				'name' => strtolower($fileinfo->getBasename('.' . $fileinfo->getExtension())),
+				'type' => strtolower($fileinfo->getExtension())
+			];
+		}
+		return $files;
+	}
 	public function loadFile(string $filename)
 	{
 		if (!file_exists(realpath($filename))) {
@@ -54,7 +54,7 @@ class Config
 		$file = (object) ['name' => $info['filename'], 'filename' => $filename, 'type' => strtolower($info['extension'])];
 		$name = $file->name;
 		$data = $this->getData($file);
-    $this->add($name, $data);
+		$this->add($name, $data);
 		return true;
 	}
 	protected function getData($file)
@@ -71,58 +71,58 @@ class Config
 		}
 	}
 	protected function set($name, $value)
-  {
-    $this->data[$name] = $value;
-    if (is_array($value)) {
-      foreach ($value as $key => $val) {
-        $n = $name . '.' . $key;
-        $this->add($n, $val);
-      }
-    }
-  }
+	{
+		$this->data[$name] = $value;
+		if (is_array($value)) {
+			foreach ($value as $key => $val) {
+				$n = $name . '.' . $key;
+				$this->add($n, $val);
+			}
+		}
+	}
 	public function add($field, $value)
 	{
 		$this->set($field, $value);
 		$this->checkValues();
 	}
 	protected function checkValues($array = null)
-  {
-    if ($array == null) {
-      foreach ($this->data as $key => $config) {
-        $this->data[$key] = $this->checkValues($config);
-      }
-      return;
-    }
-    if (is_array($array)) {
-      foreach ($array as $key => $val) {
-        $array[$key] = $this->checkValues($val);
-      }
-      return $array;
-    }
-    return $this->replace($array);
-  }
+	{
+		if ($array == null) {
+			foreach ($this->data as $key => $config) {
+				$this->data[$key] = $this->checkValues($config);
+			}
+			return;
+		}
+		if (is_array($array)) {
+			foreach ($array as $key => $val) {
+				$array[$key] = $this->checkValues($val);
+			}
+			return $array;
+		}
+		return $this->replace($array);
+	}
 	protected function replace($value)
-  {
-    if (is_array($value)) {
+	{
+		if (is_array($value)) {
 			foreach ($value as $k => $v) {
 				$value[$k] = $this->replace($v);
 			}
 			return $value;
 		}
-    if (strpos($value, '{') !== false) {
-  		while(strpos($value, '{') !== false) {
-  			$ini = strpos($value, '{') + 1;
-  			$end = strpos($value, '}', $ini);
-  			$rep = substr($value, $ini, $end - $ini);
-        $get = $this->get($rep);
-        if (strpos($get, '{') !== false) {
-          $get = $this->replace($get);
-        }
-  			$value = str_replace('{' . $rep . '}', $get, $value);
-  		}
-    }
-    return $value;
-  }
+		if (strpos($value, '{') !== false) {
+			while(strpos($value, '{') !== false) {
+				$ini = strpos($value, '{') + 1;
+				$end = strpos($value, '}', $ini);
+				$rep = substr($value, $ini, $end - $ini);
+				$get = $this->get($rep);
+				if (strpos($get, '{') !== false) {
+					$get = $this->replace($get);
+				}
+				$value = str_replace('{' . $rep . '}', $get, $value);
+			}
+		}
+		return $value;
+	}
 
 	public function get($name = null)
 	{
@@ -135,17 +135,17 @@ class Config
 		return null;
 	}
 	public function remove($name)
-  {
-    $keys = explode('.', $name);
-    for ($i = 0; $i < count($keys); $i ++) {
-      $n = implode('.', array_slice($keys, 0, $i + 1));
-      $str = "unset(\$this->data['{$n}']";
-      for ($j = $i + 1; $j < count($keys); $j ++) {
-        $str .= "['{$keys[$j]}']";
-      }
-      $str .= ');';
-      eval($str);
-    }
-  }
+	{
+		$keys = explode('.', $name);
+		for ($i = 0; $i < count($keys); $i ++) {
+			$n = implode('.', array_slice($keys, 0, $i + 1));
+			$str = "unset(\$this->data['{$n}']";
+			for ($j = $i + 1; $j < count($keys); $j ++) {
+				$str .= "['{$keys[$j]}']";
+			}
+			$str .= ');';
+			eval($str);
+		}
+	}
 }
 ?>
